54a6e7202caf3b256a2bf95070b4411ecfb5bf57
HannaMois <hannamois135@gmail.com>
2022-11-25 00:56:50 +0200

    Создаю new-swiper.js

---
+++ src/js/new-swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+        // spaceBetween: 24,
+        // grabCursor: true,
+        speed: 3000,
+        effect: 'fade',
+      
+        autoplay: {
+        delay: 3800,
+        disableOnInteraction: false,
+}
+});
