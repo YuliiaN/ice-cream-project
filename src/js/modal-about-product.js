77cac1f76cd95bfda86ca68b761af2ceeb314cfb
Lina Budkova <linabudkova35@gmail.com>
2022-11-24 19:19:37 +0200

    add modal js

---
+++ src/js/modal-about-product.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-about-product]'),
+    closeModalBtn: document.querySelector('[data-modal-close-about-product]'),
+    modal: document.querySelector('[data-modal-about-product]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
