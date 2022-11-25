373edf18d735c25abda1e9050913f4b814b9fae6
Lina Budkova <linabudkova35@gmail.com>
2022-11-25 16:24:24 +0200

    add js

--- src/js/modal-buy-now.js
+++ src/js/modal-buy-now.js
@@ -1,11 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-buy-now]"),
+    openModalBtn1: document.querySelector("[data-modal-open-buy-now-mob]"),
+    openModalBtn2: document.querySelector("[data-modal-open-buy-now-tablet]"),
+    openModalBtn3: document.querySelector("[data-modal-open-buy-now-desk]"),
     closeModalBtn: document.querySelector("[data-modal-close-buy-now]"),
     modal: document.querySelector("[data-modal-buy-now]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn1.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
+  refs.openModalBtn3.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
