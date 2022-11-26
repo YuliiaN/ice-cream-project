1d65221eaa5eb06d256d8b45ea716508c90b8f44
Lina Budkova <linabudkova35@gmail.com>
2022-11-26 18:06:38 +0200

    qwe

--- src/js/modal-about-product.js
+++ src/js/modal-about-product.js
@@ -1,11 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-about-product]'),
+    openModalBtn1: document.querySelector('[data-modal-open-about-product-one]'),
+    openModalBtn2: document.querySelector('[data-modal-open-about-product-two]'),
+    openModalBtn3: document.querySelector('[data-modal-open-about-product-tree]'),
     closeModalBtn: document.querySelector('[data-modal-close-about-product]'),
     modal: document.querySelector('[data-modal-about-product]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.openModalBtn3.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
